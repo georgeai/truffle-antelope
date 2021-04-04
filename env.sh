9319c652124d9129afeb22aa91e5172a2f058a20
George Shih <george@chez.ai>
2021-04-04 17:32:22 -0400

    use zsh so actually sets env vars, also echo

--- env.sh
+++ env.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+#!/bin/zsh
 export MNEMONIC='kitten warrior chaos sight wedding rhythm repair dawn also obvious novel oppose'
 export INFURA_PROJECT_ID=6393bde0835e430a8ea40f4594e320c5
+echo "MNEMONIC=$MNEMONIC"
+echo "INFURA_PROJECT_ID=$INFURA_PROJECT_ID"
